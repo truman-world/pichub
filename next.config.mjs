adb061bfe3d67af34c497615fea8a8291076ff86
truman <68187484+truman-world@users.noreply.github.com>
2025-07-02 18:12:14 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // --- 核心修改：告诉Next.js输出一个独立的、包含所有依赖的文件夹 ---
-  // 这一行是解决 "cannot stat '.next/standalone'" 错误的关键
+  // 这一行是解决 ".next/standalone directory not found" 错误的唯一关键
   output: 'standalone',
 };
 

7ede3a05e8e83ade5df536081550e2f777b23605
truman <68187484+truman-world@users.noreply.github.com>
2025-07-02 18:01:43 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
-
-  // 这个配置是解决图片404问题的关键。
-  // 它告诉 Next.js，任何对 /uploads/* 路径的请求，
-  // 实际上都应该去服务器的一个稳定、可公开访问的目录去寻找文件。
-  async rewrites() {
-    return [
-      {
-        source: '/uploads/:path*',
-        destination: '/.next/static/media/:path*',
-      },
-    ]
-  },
+  // --- 核心修改：告诉Next.js输出一个独立的、包含所有依赖的文件夹 ---
+  output: 'standalone',
 };
 
 export default nextConfig;

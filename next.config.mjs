80264748a15ec67e0f2a8c5e92d24ceed319a754
truman <68187484+truman-world@users.noreply.github.com>
2025-07-02 18:06:25 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // --- 核心修改：告诉Next.js输出一个独立的、包含所有依赖的文件夹 ---
+  // 这一行是解决 "cannot stat '.next/standalone'" 错误的关键
   output: 'standalone',
 };
 

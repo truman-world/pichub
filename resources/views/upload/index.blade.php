4fc2a99cb1dfb9e9626dae0193d33d9705b8ee3e
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 23:05:46 +0800

    Create index.blade.php

---
+++ resources/views/upload/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <h1 class="text-2xl font-semibold text-gray-900 dark:text-gray-100 mb-6">
+        Upload Images
+    </h1>
+    
+    <x-upload-zone />
+</div>
+@endsection

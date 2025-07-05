cfb2ac1fbe24ea2ee2b80ea27eaba13412497899
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 09:48:01 +0800

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // 我们只需要这一个配置，告诉Next.js生成适合“动静分离”部署的输出。
-  // 这一行是解决 ".next/standalone directory not found" 错误的唯一关键。
-  output: 'standalone',
-};
-
-export default nextConfig;

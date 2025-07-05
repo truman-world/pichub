ad7908b5740670a0e8a3ffa4054b30047a332e9a
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 14:42:28 +0800

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// API routes will be added in Phase 3

84fa7b9258fb5366481522a4f34eb729a0d19c6d
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 08:14:28 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 我们只需要这一个配置，告诉Next.js生成适合“动静分离”部署的输出。
-  // 不要添加 basePath。
-  output: 'standalone',
+  // 我们不再需要任何特殊配置
 };
 
 export default nextConfig;

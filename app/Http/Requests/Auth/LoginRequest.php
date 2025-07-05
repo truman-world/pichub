377d6770ae272a1516b0ca7c9217b40bb6919e13
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 14:48:18 +0800

    Create LoginRequest.php

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email', 'max:100'],
+            'password' => ['required', 'string'],
+            'remember' => ['boolean'],
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => __('validation.required', ['attribute' => __('auth.email')]),
+            'email.email' => __('validation.email', ['attribute' => __('auth.email')]),
+            'password.required' => __('validation.required', ['attribute' => __('auth.password')]),
+        ];
+    }
+}

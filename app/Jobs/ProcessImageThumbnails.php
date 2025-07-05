84917c8f3442853de807375ec4f12bc73aec1932
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 23:07:16 +0800

    Create ProcessImageThumbnails.php

---
+++ app/Jobs/ProcessImageThumbnails.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Image;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessImageThumbnails implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(
+        public Image $image
+    ) {}
+
+    public function handle(): void
+    {
+        // Thumbnail processing logic will be implemented here
+    }
+}

dab895dd66903b97578cf037c1ab3bf2b6e5c192
truman <68187484+truman-world@users.noreply.github.com>
2025-07-06 07:58:01 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    /**
+     * Display the home page.
+     */
+    public function index(): View
     {
         return view('welcome');
     }

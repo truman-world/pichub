c3c17e04cb577a30f8d5533859c0c0463aa1bcf8
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 20:55:07 +0800

    Create SendWelcomeEmail.php

---
+++ app/Jobs/SendWelcomeEmail.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendWelcomeEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public User $user
+    ) {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // Send welcome email
+        // Mail::to($this->user)->send(new WelcomeMail($this->user));
+    }
+}

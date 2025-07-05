2102d91a8b40680a5670d1a7b51f90f396a45db4
truman <68187484+truman-world@users.noreply.github.com>
2025-07-05 22:49:42 +0800

    Create UploadController.php

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,15 @@
+cat > app/Http/Controllers/HomeController.php << 'EOF'
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+}
+EOF
